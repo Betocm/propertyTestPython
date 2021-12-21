c1905f5d38098820c31ad9e44936ecb9d80f0bcb
Alberto Calderon <insolation.ac@gmail.com>
2021-12-20 22:00:30 -0800

    First upload of the project

---
+++ property_data/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def property_list(request):
+    return
+
+def property_form(request):
+    return
+
+def property_delete(request):
+    return
